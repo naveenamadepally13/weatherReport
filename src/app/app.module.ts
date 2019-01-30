ceab465a1720bfceaf2a62d045f3080fdc15658c
naveenamadepally13 <47007814+naveenamadepally13@users.noreply.github.com>
2019-01-30 10:46:13 -0600

    added weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { WeatherComponent } from './weather/weather.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
